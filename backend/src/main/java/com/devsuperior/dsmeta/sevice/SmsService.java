d441471aeb7189eb811fc539f88e0082494ec869
IsaacSkywalker <isaac.sky.01@hotmail.com>
2022-07-17 11:18:15 -0300

    Send notification

--- backend/src/main/java/com/devsuperior/dsmeta/sevice/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/sevice/SmsService.java
@@ -38,7 +38,7 @@ public class SmsService {
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
 		/*Definido a mensagem que irá aparecer no sms*/
-		String msg = "O Vendedor " + sale.getSellerName() + "foi destaque em " + date
+		String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " + date
 				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,30 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from "../../utils/request";
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+/**Função que irá chamar a requisição API com o parâmetro definido na função*/
+function handClick(id: number) {
+    /**"Axios" biblioteca para fazer a requisição de dados no back end da aplicação. Devemos passar
+     * o endereço URL definido nas variáveis de ambiente e se o retorno for true, esse retorno será
+     * armazenado na variárel "response" do ".then". Toda vez que quisermos o retorno verdadeiro de
+     * uma função, deveremos utilizar o ".then".
+    */
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("sucesso")
+        })
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-        <div className="dsmeta-red-btn">
+        /**"onClick" atribuindo ação de click ao botão. Essa ação necessita como argumento uma função
+         * "()" => nome da função (parâmetro).
+        */
+        <div className="dsmeta-red-btn" onClick={() => handClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
